a120f6fe6117725267156ada57fbeb7786ba46ac
Adam Braus <ajbraus@gmail.com>
2015-10-20 12:38:18 -0700

    adding auth

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var handlebars  = require('express-handlebars');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var session = require('express-session');
 
 var app = express();
 
@@ -18,11 +19,55 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static('public'));
 
+app.use(session({
+  saveUninitialized: true,
+  resave: true,
+  secret: 'SuperSecretCookie',
+  cookie: { maxAge: 600000 }
+}));
+
 mongoose.connect('mongodb://localhost/express-start');
 var Todo = require('./models/todo.js');
+var User = require('./models/user.js');
+
+app.get('/signup', function (req, res) {
+	res.render('signup');
+})
+
+app.post('/users', function (req, res) {
+	var user = req.body;
+	User.createSecure(user.email, user.password, function (err, user) {
+		req.session.user = user;
+		res.json({ user: user, msg: "User Created Successfully!" });
+	})
+})
+
+app.post('/login', function (req, res) {
+	var user = req.body;
+	User.authenticate(user.email, user.password, function (err, user) {
+		if (err) { console.log("there was an err: ", err) }
+		req.session.user = user;
+		res.json(user);
+	})
+})
+
+app.get('/current-user', function (req, res) {
+	res.json({ user: req.session.user })
+})
+
+app.get('/logout', function (req, res) {
+	req.session.user = null;
+
+	res.json({ msg: "User Logged Out Successfully!"})
+})
+
+app.get('/login', function (req, res) {
+	res.render('login');
+})
 
 // TODOS INDEX
 app.get('/', function (req, res) {
+	console.log(req.session.userId);
 	Todo.find().exec(function (err, todos) {
 		res.render('home', { todos: todos });
 	})
@@ -51,6 +96,7 @@ app.delete('/todos/:id', function (req, res) {
 		res.status(200).json({});
 	})
 })
+
 // TODOS UPDATE
 
 // TODOS EDIT
